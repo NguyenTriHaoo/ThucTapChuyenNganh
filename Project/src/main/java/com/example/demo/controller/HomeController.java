a09e3c726d16f0802c45678dd076e24866dbcbda
DH52200637-NguyenTriHao <haonguyen27042004@gmail.com>
2025-10-29 14:11:12 +0700

    UP FILE Project

---
+++ Project/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,32 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/storage")
+    public String storage(){
+        return "storage";
+    }
+
+    @GetMapping("/product")
+    public String product(){
+        return "product";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+}
