9662c0248e4ec43e7c860f358801f9d71bb48684
DH52200637-NguyenTriHao <haonguyen27042004@gmail.com>
2025-11-08 22:03:32 +0700

    Them Layout cua shop va login

---
+++ Project/src/main/java/com/example/demo/controller/AdminController.java
@@ -0,0 +1,57 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AdminController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/admin/Product-main/category")
+    public String categoryList(){
+        return "admin/Product-main/category-list";
+    }
+
+    @GetMapping("/admin/Product-main/product")
+    public String productList(){
+        return "admin/Product-main/product-list";
+    }
+
+    @GetMapping("/admin/Management-main/menu")
+    public String MenuList(){
+        return "admin/Management-main/menu-list";
+    }
+
+    @GetMapping("/admin/Management-main/role")
+    public String roleList(){
+        return "admin/Management-main/role-list";
+    }
+
+    @GetMapping("/admin/Management-main/user")
+    public String userList(){
+        return "admin/Management-main/user-list";
+    }
+
+    @GetMapping("/admin/Storage-main/add-storage")
+    public String addStorage(){
+        return "admin/Storage-main/add-storage-list";
+    }
+
+    @GetMapping("/admin/Storage-main/export-storage")
+    public String exportStorage(){
+        return "admin/Storage-main/add-storage-list";
+    }
+
+    @GetMapping("/admin/Storage-main/storage-history")
+    public String StorageHistory(){
+        return "admin/Storage-main/storage-history";
+    }
+
+    @GetMapping("/admin/Storage-main/export-list")
+    public String StorageList(){
+        return "admin/Storage-main/storage-list";
+    }
+}
--- Project/src/main/java/com/example/demo/controller/HomeController.java
+++ Project/src/main/java/com/example/demo/controller/HomeController.java
@@ -5,28 +5,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    public String index(){
-        return "index";
+    @GetMapping("/home")
+    public String indexnhacungcap(){
+        return "indexnhacungcap";
     }
 
-    @GetMapping("/about")
-    public String about(){
-        return "about";
+    @GetMapping("/shop")
+    public String Shop(){
+        return "home/shop";
     }
 
-    @GetMapping("/storage")
-    public String storage(){
-        return "storage";
-    }
-
-    @GetMapping("/product")
-    public String product(){
-        return "product";
-    }
-
-    @GetMapping("/admin")
-    public String admin(){
-        return "admin";
+    @GetMapping("/feature")
+    public String feature(){
+        return "home/feature";
     }
 }
