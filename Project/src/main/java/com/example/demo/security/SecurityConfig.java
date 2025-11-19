15426803712a70e4fe6c68525f1201a651b6b3dc
DH52200637-NguyenTriHao <haonguyen27042004@gmail.com>
2025-11-19 11:31:38 +0700

    Hoàn thành login logout

---
+++ Project/src/main/java/com/example/demo/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails hao = User.builder()
+                .username("hao")
+                .password("{noop}123456")
+                .roles("EMPLOYEE")
+                .build();
+        return new InMemoryUserDetailsManager(hao);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer->
+                        configurer.requestMatchers(
+                                        "/css/**",
+                                        "/js/**",
+                                        "/images/**",
+                                        "/fonts/**",
+                                        "/favicon.ico",
+                                        "/json/**",
+                                        "/vendor/**"
+                                ).permitAll()
+                        .anyRequest().authenticated()).formLogin(
+                form->
+                            form
+                                    .loginPage("/login")
+                                    .defaultSuccessUrl("/")
+                                    .loginProcessingUrl("/authenticateTheUser")
+                                    .permitAll()
+        )
+                .logout(logout->logout.permitAll()
+
+
+                );
+
+                return http.build();
+    }
+}
